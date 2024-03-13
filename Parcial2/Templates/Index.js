8fcae7e08fccc3ac3e646b1b540d3fd4195f9520
FranciscoJavierAmaroRodriguez <pacoempires@outlook.com>
2024-03-13 09:57:35 -0500

    Trabajo en clase

---
+++ Parcial2/Templates/Index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+app.set('view engine', 'pug')
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/', (req, res) => {
+res.render('index.pug',{nombre: "Francisco", apellido: "Amaro"});
+});
+
+
+app.listen(3000, () => {
+console.log('Example app listening on port 3000!');
+});
