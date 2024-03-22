113d1e4c225f3ac7b2d2b64fc1204d52d8727941
FranciscoJavierAmaroRodriguez <pacoempires@outlook.com>
2024-03-21 19:22:54 -0500

    ServerFormData

---
+++ Parcial2/ServerFormData/index.js
@@ -0,0 +1,28 @@
+const multer = require("multer");
+const express = require("express")
+
+const storage = multer.diskStorage({
+    destination: "./Parcial2/Multer/uploads/",
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+  
+  const upload = multer({ storage: storage });
+  const app = express();
+
+app.post("/uploads", upload.single("file"),(req,res,next) =>{
+res.send("Archivo subido");
+});
+    
+app.use((req, res ,next) =>{
+    res.status(404).send("Url not found");
+});
+
+app.use((err, req, res ,next) =>{
+    res.status(500).json({error: err.message});
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+    });
